8d3e4b487a6a5600113fba3fbfcf8eab4992d9d9
mahendraempyreal <mahendra.empyreal@gmail.com>
2023-09-26 17:50:36 +0530

    new way package added

--- src/shared/BlankTask.js
+++ src/shared/BlankTask.js
@@ -1,17 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Image from "next/image";
-import NoTaskImg from "../images/no-task.png";
 
 function BlankTask({ tab }) {
   return (
     <div className="m-auto my-0 rounded-lg bg-white shadow-cardv2 p-4 ht-inherit">
-      <Image
-        width={100}
-        height={100}
-        src={NoTaskImg}
-        className="w-auto mb-1 max-h-12 mx-auto mt-[40%]"
-      />
       <div className="bg-white-whitest p-2 rounded-lg text-secondary-light1 font-normal text-xs text-center mx-auto my-2.5">
         {tab === "status"
           ? "No visible tasks in the defined dates."
