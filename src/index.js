fc5395892be3ef26294a7f54118086b323c0d085
mahendraempyreal <mahendra.empyreal@gmail.com>
2023-09-26 11:47:09 +0530

    babel script added in package file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /* export * from'./shared/organisms/CheckHtml.js';
 export { default as NoRoleAdded} from'./shared/organisms/NoRoleAdded'; */
-export * from "./shared/organisms/CheckHtml.js";
-export * from "./shared/organisms/NoRoleAdded.js";
+/* export * from "./shared/organisms/CheckHtml.js";
+export * from "./shared/organisms/NoRoleAdded.js"; */
 
-/* import CheckHtml from "./shared/organisms/CheckHtml";
+import CheckHtml from "./shared/organisms/CheckHtml";
 import NoRoleAdded from "./shared/organisms/NoRoleAdded";
+/* const NoRoleAdded = require("./shared/organisms/NoRoleAdded");
+const CheckHtml = require("./shared/organisms/CheckHtml");
 
-export { CheckHtml, NoRoleAdded }; */
-export {};
+module.exports = { NoRoleAdded, CheckHtml }; */
+export { CheckHtml, NoRoleAdded };
